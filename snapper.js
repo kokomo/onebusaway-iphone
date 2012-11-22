170a4006389077d0e4fa2d5c01de3fbd2e3a3cd0
Aaron Brethorst <aaron@brethorsting.com>
2012-11-21 16:00:07 -0800

    more cleanup

--- snapper.js
+++ snapper.js
@@ -6,19 +6,34 @@ var target = UIATarget.localTarget();
 var app = target.frontMostApp();
 var window = app.mainWindow();
 
+// requires a number or null (e.g. 1, 0 or null -> yes, no, or 'unknown')
+var triStateBooleanValue = function(val) {
+  if (val > 0) {
+    return true;
+  } else if (val == 0) {
+    return false;
+  } else {
+    return null;
+  }
+};
+
 var dumpJSON = function(elt) {
   if (elt.isNil()) {
     return null;
   }
 
-  return {
+  var eltData = {
     label: elt.label(),
     name: elt.name(),
     value: elt.value(),
     rect: elt.rect(),
     hint: elt.hint(),
+    isValid: elt.isValid(),
+    isVisible: triStateBooleanValue(elt.isVisible()),
     stringValue: elt.toString()
   };
+
+  return eltData;
 };
 
 var dumpJSONTree = function(elt) {
